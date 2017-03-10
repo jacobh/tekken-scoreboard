6bc366f664b0aeca1de4688a566a769c8e84036f
Jacob Haslehurst <jacob@haslehurst.net>
2017-03-10 20:01:51 +1100

    add uuid module, add character endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import morgan from "morgan";
+import uuidV4 from "uuid/v4";
+
+import staticData from "../data.json";
+
+console.log(staticData);
 
 var app = express();
 
@@ -7,6 +12,10 @@ app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("hi");
+});
+
+app.get("/character/", (req, res) => {
+  res.send(staticData);
 });
 
 app.listen(3000);

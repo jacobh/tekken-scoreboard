ff345f0d75aa831de7ca9396901993b27c06f090
Jacob Haslehurst <jacob@haslehurst.net>
2017-03-17 22:43:02 +1100

    add bootstrap scaffold

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+import { Grid, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import data from "../../data.json";
 
 class App extends Component {
   render() {
     return (
-      <div className="App"><pre>{JSON.stringify(data, null, 2)}</pre></div>
+      <Grid>
+        <Row>
+          <Col>
+            <div className="App">
+              <pre>{JSON.stringify(data, null, 2)}</pre>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 }

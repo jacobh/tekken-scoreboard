1a6b0f67adf6d284555f7f25e345ae8f087b88e5
Jacob Haslehurst <jacob@haslehurst.net>
2017-03-18 05:28:07 +1100

    add tekken sets to db

--- backend/models.js
+++ backend/models.js
@@ -21,8 +21,23 @@ export const Character = sequelize.define("character", {
   name: DataTypes.STRING
 });
 
+export const TekkenSet = sequelize.define("tekken_set", {
+  id: {
+    primaryKey: true,
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4
+  }
+});
+
+TekkenSet.belongsTo(Player, { as: "winner" });
+TekkenSet.belongsTo(Player, { as: "player1" });
+TekkenSet.belongsTo(Player, { as: "player2" });
+TekkenSet.belongsTo(Character, { as: "character1" });
+TekkenSet.belongsTo(Character, { as: "character2" });
+
 Player.sync();
 Character.sync();
+TekkenSet.sync();
 
 // load initial data
 for (let char of staticData.characters) {
@@ -36,5 +51,18 @@ for (let player of staticData.players) {
   Player.findOrCreate({
     where: { id: player.uuid },
     defaults: { name: player.name }
+  });
+}
+
+for (let set of staticData.sets) {
+  TekkenSet.findOrCreate({
+    where: { id: set.uuid },
+    defaults: {
+      winnerId: set.winnerId,
+      player1Id: set.player1Id,
+      player2Id: set.player2Id,
+      character1Id: set.character1Id,
+      character2Id: set.character2Id
+    }
   });
 }

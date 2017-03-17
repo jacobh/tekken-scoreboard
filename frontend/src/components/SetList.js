728b140527608af40df5d8aa34dedc42e24a3b9b
Jacob Haslehurst <jacob@haslehurst.net>
2017-03-18 03:49:22 +1100

    display player names and highlight winner

--- frontend/src/components/SetList.js
+++ frontend/src/components/SetList.js
@@ -1,13 +1,32 @@
 // @flow
 import React from "react";
 import * as Immutable from "immutable";
+import classNames from "classnames";
 import { connect } from "react-redux";
 
 function SetList(
   props: {
-    sets: Immutable.Map<string, Immutable.Map<string, any>>
+    sets: Immutable.Map<string, Immutable.Map<string, any>>,
+    players: Immutable.Map<string, Immutable.Map<string, any>>
   }
 ) {
+  const displaySets = props.sets.toIndexedSeq().map(set => {
+    const winnerId = set.get("winnerId");
+
+    let winner;
+    if (winnerId === set.get("player1Id")) {
+      winner = "PLAYER_1";
+    } else if (winnerId === set.get("player2Id")) {
+      winner = "PLAYER_2";
+    }
+
+    return Immutable.Map({
+      uuid: set.get("uuid"),
+      winner: winner,
+      player1Name: props.players.get(set.get("player1Id")).get("name"),
+      player2Name: props.players.get(set.get("player2Id")).get("name")
+    });
+  });
   return (
     <div>
       <h2>Past Sets</h2>
@@ -19,10 +38,22 @@ function SetList(
           </tr>
         </thead>
         <tbody>
-          {props.sets.toIndexedSeq().map(set => (
+          {displaySets.map(set => (
             <tr key={set.get("uuid")}>
-              <td>{set.get("player1Id")}</td>
-              <td>{set.get("player2Id")}</td>
+              <td
+                className={classNames({
+                  success: set.get("winner") === "PLAYER_1"
+                })}
+              >
+                {set.get("player1Name")}
+              </td>
+              <td
+                className={classNames({
+                  success: set.get("winner") === "PLAYER_2"
+                })}
+              >
+                {set.get("player2Name")}
+              </td>
             </tr>
           ))}
         </tbody>
@@ -33,6 +64,7 @@ function SetList(
 
 export default connect(state => {
   return {
-    sets: state.get("sets")
+    sets: state.get("sets"),
+    players: state.get("players")
   };
 })(SetList);

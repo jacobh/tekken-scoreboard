49481a8e1b7497bbe40217dd94fcd38bd0dbec21
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-01 19:23:47 +1100

    split out bootstrap script

---
+++ backend/bootstrap.js
@@ -0,0 +1,31 @@
+// @flow
+import staticData from "../data.json";
+import { Player, Character, Match } from "./models.js";
+
+// load initial data
+for (let char of staticData.characters) {
+  Character.findOrCreate({
+    where: { id: char.uuid },
+    defaults: { name: char.name }
+  });
+}
+
+for (let player of staticData.players) {
+  Player.findOrCreate({
+    where: { id: player.uuid },
+    defaults: { name: player.name }
+  });
+}
+
+for (let match of staticData.matches) {
+  Match.findOrCreate({
+    where: { id: match.uuid },
+    defaults: {
+      winnerId: match.winnerId,
+      player1Id: match.player1Id,
+      player2Id: match.player2Id,
+      character1Id: match.character1Id,
+      character2Id: match.character2Id
+    }
+  });
+}
--- backend/models.js
+++ backend/models.js
@@ -1,7 +1,6 @@
 // @flow
 import { DataTypes } from "sequelize";
 import { sequelize } from "./database.js";
-import staticData from "../data.json";
 
 export const Player = sequelize.define("player", {
   id: {
@@ -38,31 +37,3 @@ Match.belongsTo(Character, { as: "character2" });
 Player.sync();
 Character.sync();
 Match.sync();
-
-// load initial data
-for (let char of staticData.characters) {
-  Character.findOrCreate({
-    where: { id: char.uuid },
-    defaults: { name: char.name }
-  });
-}
-
-for (let player of staticData.players) {
-  Player.findOrCreate({
-    where: { id: player.uuid },
-    defaults: { name: player.name }
-  });
-}
-
-for (let match of staticData.matches) {
-  Match.findOrCreate({
-    where: { id: match.uuid },
-    defaults: {
-      winnerId: match.winnerId,
-      player1Id: match.player1Id,
-      player2Id: match.player2Id,
-      character1Id: match.character1Id,
-      character2Id: match.character2Id
-    }
-  });
-}

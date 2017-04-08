6c49df49eee981fba2fb449cd1da02c2809c0b65
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-08 16:28:12 +1000

    remove everything to do with redux

--- frontend/src/actions.js
+++
@@ -1,5 +0,0 @@
-// @flow
-import { createAction } from "redux-actions";
-
-export const createMatch = createAction("CREATE_MATCH", match => match);
-export const loadMatch = createAction("LOAD_MATCH", match => match);
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
-import { Provider } from "react-redux";
 import App from "./App";
-import store from "./store.js";
 import client from "./apolloClient.js";
 import "./index.css";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </ApolloProvider>,
   document.getElementById("root")
 );

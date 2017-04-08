b13ca448590d0eac25b7e4af4aeb0d7528658c61
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-08 14:44:53 +1000

    Implement MatchList with graphql

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,18 @@
 // @flow
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
 import { Provider } from "react-redux";
 import App from "./App";
 import store from "./store.js";
+import client from "./apolloClient.js";
 import "./index.css";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById("root")
 );

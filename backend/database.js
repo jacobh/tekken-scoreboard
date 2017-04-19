aff80c84c319b12a168b23c2484fbe90b6598886
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-19 15:35:57 +1000

    use database url from environment

--- backend/database.js
+++ backend/database.js
@@ -1,6 +1,4 @@
 // @flow
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize(
-  "postgres://jacobhaslehurst@localhost:5432/tekken_scoreboard"
-);
+export const sequelize = new Sequelize(process.env.DATABASE_URL);

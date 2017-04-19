6a7463dca6dd7501a67d7dce82e018af8fc53827
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-19 18:56:51 +1000

    only use ssl for postgres if enabled

--- backend/database.js
+++ backend/database.js
@@ -4,6 +4,6 @@ import Sequelize from "sequelize";
 export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
-    ssl: true
+    ssl: process.env.PG_SSL ? true : false
   }
 });

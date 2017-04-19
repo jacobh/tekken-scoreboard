bf96bdab236619665082b005114ec1d93c768292
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-19 17:19:40 +1000

    enable ssl for database connection

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,9 @@
 // @flow
 import Sequelize from "sequelize";
 
-export const sequelize = new Sequelize(process.env.DATABASE_URL);
+export const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
+});

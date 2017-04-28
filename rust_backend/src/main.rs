af318aff82d58851ad38832c17296e4e20e5aa6f
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-28 20:56:47 +1000

    super basic echo webserver

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,3 +1,29 @@
+extern crate iron;
+extern crate router;
+extern crate logger;
+extern crate env_logger;
+
+use iron::prelude::*;
+use iron::status;
+use router::Router;
+use logger::Logger;
+use logger::Format;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init().unwrap();
+    let (logger_before, logger_after) = Logger::new(Some(Format::default()));
+
+    let mut router = Router::new();
+    router.get("/graphql", handler, "graphql");
+
+    let mut chain = Chain::new(router);
+
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+
+    Iron::new(chain).http("localhost:3000").unwrap();
+
+    fn handler(req: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Hi")))
+    }
 }

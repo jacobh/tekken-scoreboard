d4685297f55e51ee47fad329f492ef93ec93a7d4
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-28 21:11:15 +1000

    add schema structs

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -1,13 +1,42 @@
 extern crate iron;
+extern crate chrono;
 extern crate router;
 extern crate logger;
 extern crate env_logger;
+extern crate uuid;
 
 use iron::prelude::*;
+use chrono::prelude::*;
 use iron::status;
 use router::Router;
 use logger::Logger;
 use logger::Format;
+use uuid::Uuid;
+
+struct Player {
+    id: Uuid,
+    name: String,
+    matches: Vec<Match>,
+    played_matches: u32,
+    won_matches: u32,
+    lost_matches: u32,
+}
+
+struct Character {
+    id: Uuid,
+    name: String,
+}
+
+struct Match {
+    id: Uuid,
+    created_at: DateTime<UTC>,
+    winner: Player,
+    loser: Player,
+    player1: Player,
+    player2: Player,
+    character1: Character,
+    character2: Character,
+}
 
 fn main() {
     env_logger::init().unwrap();

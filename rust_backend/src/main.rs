d1791a7a75d9df3b475d1f232fb828c938189fd5
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-29 12:24:56 +1000

    implement pg connection pool, all_characters lookup via db

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -4,22 +4,37 @@ extern crate mount;
 extern crate logger;
 extern crate env_logger;
 extern crate uuid;
+extern crate r2d2;
+extern crate r2d2_postgres;
+extern crate postgres;
 #[macro_use]
 extern crate juniper;
+extern crate persistent;
+
 
 use iron::prelude::*;
+use iron::typemap::Key;
 use chrono::prelude::*;
+use r2d2_postgres::{TlsMode, PostgresConnectionManager};
+use postgres::Connection;
 use mount::Mount;
 use logger::Logger;
 use logger::Format;
 use uuid::Uuid;
 use juniper::iron_handlers::{GraphQLHandler, GraphiQLHandler};
 use juniper::{FieldResult, Context, EmptyMutation, Value};
+use persistent::Read;
 
 struct Database {
     characters: Vec<Character>,
+    pg_pool: r2d2::Pool<PostgresConnectionManager>,
 }
 impl Context for Database {}
+
+struct PgConnPool(r2d2::Pool<PostgresConnectionManager>);
+impl Key for PgConnPool {
+    type Value = PgConnPool;
+}
 
 struct ID(Uuid);
 
@@ -81,12 +96,21 @@ graphql_object!(Character: () |&self| {
 
 struct QueryRoot;
 graphql_object!(QueryRoot: Database |&self| {
-    field all_characters(&executor) -> &Vec<Character> {
-        &executor.context().characters
+    field all_characters(&executor) -> Vec<Character> {
+        let conn = &executor.context().pg_pool.get().unwrap();
+        let mut characters: Vec<Character> = Vec::new();
+        for row in &conn.query("SELECT id, name FROM characters", &[]).unwrap() {
+            let character = Character {
+                id: ID(row.get(0)),
+                name: row.get(1)
+            };
+            &characters.push(character);
+        }
+        characters
     }
 });
 
-fn context_factory(_: &mut Request) -> Database {
+fn context_factory(req: &mut Request) -> Database {
     Database {
         characters: vec![Character { 
                              id: ID(Uuid::parse_str("52423da4-1cb1-4a69-a6bb-e351aa3bfbcb").unwrap()),
@@ -96,12 +120,18 @@ fn context_factory(_: &mut Request) -> Database {
                              id: ID(Uuid::parse_str("f1ffd139-098f-4bd6-83a1-e5b31056319a").unwrap()),
                              name: "Devil Jin".to_string(),
                          }],
+        pg_pool: req.get::<Read<PgConnPool>>().unwrap().0.clone()
     }
 }
 
 fn main() {
     env_logger::init().unwrap();
     let (logger_before, logger_after) = Logger::new(Some(Format::default()));
+
+    let pg_pool_manager = PostgresConnectionManager::new("postgres://jacobhaslehurst@localhost/tekken_scoreboard",
+                                                         TlsMode::None)
+            .unwrap();
+    let pg_pool = PgConnPool(r2d2::Pool::new(r2d2::Config::default(), pg_pool_manager).unwrap());
 
     let mut mount = Mount::new();
 
@@ -116,6 +146,7 @@ fn main() {
 
     chain.link_before(logger_before);
     chain.link_after(logger_after);
+    chain.link(Read::<PgConnPool>::both(pg_pool));
 
     Iron::new(chain).http("localhost:3000").unwrap();
 }

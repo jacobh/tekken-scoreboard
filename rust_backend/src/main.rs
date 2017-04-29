bbf13b1035a15f056db7f261607b14d9922fd950
Jacob Haslehurst <jacob@haslehurst.net>
2017-04-29 16:50:47 +1000

    configure static file serving

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -10,6 +10,7 @@ extern crate postgres;
 #[macro_use]
 extern crate juniper;
 extern crate persistent;
+extern crate staticfile;
 
 
 use iron::prelude::*;
@@ -323,11 +324,14 @@ fn context_factory(req: &mut Request) -> Database {
 fn main() {
     let mut database_url: String = "".to_string();
     let mut port: String = "4000".to_string();
+    let mut static_dir: String = "../frontend/build".to_string();
     for (key, value) in env::vars() {
         if key == "DATABASE_URL" {
             database_url = value
         } else if key == "PORT" {
             port = value
+        } else if key == "STATIC_DIR" {
+            static_dir = value
         }
     }
 
@@ -346,6 +350,7 @@ fn main() {
 
     mount.mount("/graphql", graphql_handler);
     mount.mount("/graphiql", graphiql_handler);
+    mount.mount("/", staticfile::Static::new(static_dir));
 
     let mut chain = Chain::new(mount);
 

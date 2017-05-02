ef0f3c39a3e1403b697868a0fb05c96bae857639
Jacob Haslehurst <jacob@haslehurst.net>
2017-05-02 22:24:08 +1000

    finish implementing all_elo_rows

--- rust_backend/src/main.rs
+++ rust_backend/src/main.rs
@@ -322,11 +322,11 @@ graphql_object!(EloCell: Database |&self| {
     field player(&executor) -> &Player {
         (&executor.context().players.get(&self.player_id)).unwrap()
     }
-    field score() -> &f64 {
-        &self.score
+    field score() -> f64 {
+        (self.score * 10.0).round() / 10.0
     }
-    field score_change() -> &f64 {
-        &self.score_change
+    field score_change() -> f64 {
+        (self.score_change * 10.0).round() / 10.0
     }
 });
 
@@ -345,9 +345,39 @@ graphql_object!(QueryRoot: Database |&self| {
     }
 
     field all_elo_rows(&executor) -> Vec<EloRow> {
-        // fn calc_next_elo_row(prev_row: &EloRow, match_: &Match) -> EloRow {}
+        fn calc_next_elo_row(prev_row: &EloRow, match_: &Match) -> EloRow {
+            let winner_id = match_.winner_id;
+            let loser_id = match_.loser_id();
 
-        let matches: Vec<&Match> = executor.context().matches.values().collect();
+            let winner_prev_elo = prev_row.cells.iter().find(|x| x.player_id == winner_id).unwrap().score;
+            let loser_prev_elo = prev_row.cells.iter().find(|x| &x.player_id == loser_id).unwrap().score;
+
+            let (winner_next_elo, loser_next_elo) = elo::calc_new_elos(winner_prev_elo, loser_prev_elo);
+
+            EloRow {
+                created_at: Some(match_.created_at.clone()),
+                cells: prev_row.cells.iter().map(|prev_cell| {
+                    let player_id = prev_cell.player_id.clone();
+                    let next_score = {
+                        if player_id == winner_id {
+                            winner_next_elo
+                        } else if &player_id == loser_id {
+                            loser_next_elo
+                        } else {
+                            prev_cell.score
+                        }
+                    };
+                    EloCell {
+                        player_id: player_id,
+                        score: next_score,
+                        score_change: next_score - prev_cell.score
+                    }
+                }).collect()
+            }
+        }
+
+        let mut matches: Vec<&Match> = executor.context().matches.values().collect();
+        matches.sort_by_key(|m| m.created_at.0);
         let player_ids: Vec<&Uuid> = executor.context().players.values().map(|x| &x.id).collect();
         let initial_row = EloRow {
             created_at: None,
@@ -357,7 +387,15 @@ graphql_object!(QueryRoot: Database |&self| {
                 score_change: 0.0,
             }).collect()
         };
-        vec!(initial_row)
+        let mut rows: Vec<EloRow> = vec!(initial_row);
+        for match_ in matches.iter() {
+            let row = {
+                let prev_row = rows.last().expect("There should always be one row");
+                calc_next_elo_row(prev_row, match_)
+            };
+            rows.push(row);
+        }
+        rows
     }
 
     field get_character(&executor, id: ID) -> FieldResult<&Character> {
